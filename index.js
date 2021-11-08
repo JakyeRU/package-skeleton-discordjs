f239a4eb68bbd6bdd2bef908ef6f01caf0bd0009
Jakye <jakyeru@gmail.com>
2021-11-09 00:51:01 +0200

    Checking if the token is set before running the application.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 const { ShardingManager } = require('discord.js');
 
+if (!process.env.DISCORD_AUTH_TOKEN) {
+    console.error('[ERROR] Please provide a Discord authentication token!');
+    process.exit(1);
+}
+
 const manager = new ShardingManager('./bot.js', {
     token: process.env.DISCORD_AUTH_TOKEN
 });
