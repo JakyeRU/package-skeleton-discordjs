01ae0e746f7ac2763bad12fa2b9ad95f2fa3d0a5
Jakye <jakyeru@gmail.com>
2021-11-18 18:03:15 +0200

    Finished interactionCreate.js.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,6 @@
 const {Client, CommandInteraction} = require("discord.js");
+const Embed = require('../helpers/embed');
+const embed = new Embed();
 /**
  * Listens for the interactionCreate event and tries to respond to the interaction.
  * @type {{once: boolean, name: string, execute(*, *)}}
@@ -11,7 +13,23 @@ module.exports = {
      * @param {Client} client
      * @param {CommandInteraction} interaction
      */
-    execute(client, interaction) {
+    execute(interaction, client) {
+        // Making sure the interaction is a command interaction.
+        if (!interaction.isCommand()) return false;
 
+        // Making sure the command is bound to a file.
+        if (client.commands.has(interaction.commandName)) {
+            interaction.reply({
+                embeds: [embed.error('NOT_BOUND')],
+                ephemeral: true
+            }).catch(console.error);
+            return false;
+        }
+
+        // Getting the command file.
+        const command = client.commands.get(interaction.commandName);
+
+        // Executing the command.
+        command.execute(interaction).catch(console.error);
     }
 }
